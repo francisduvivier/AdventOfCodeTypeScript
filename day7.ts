5bd2dd9fdf73781a5618560f6e175303f7dd93ca
Francis Duvivier <francisduvivier@gmail.com>
2019-12-07 07:04:55 +0100

    ♻ Extract Day 7 Part 1 into function

--- day7.ts
+++ day7.ts
@@ -3,10 +3,8 @@ import {outputs, runFromMem, setStdInput} from "./intcode/IntCodeRunner";
 const input = [3, 8, 1001, 8, 10, 8, 105, 1, 0, 0, 21, 34, 43, 60, 81, 94, 175, 256, 337, 418, 99999, 3, 9, 101, 2, 9, 9, 102, 4, 9, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 102, 4, 9, 9, 1001, 9, 4, 9, 102, 3, 9, 9, 4, 9, 99, 3, 9, 102, 4, 9, 9, 1001, 9, 2, 9, 1002, 9, 3, 9, 101, 4, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 4, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 99, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99]
 
 const phases = [0, 1, 2, 3, 4];
-// const nbAmps = 5;
-let highestThrust = 0;
-let bestPhasesSetting = [0, 0, 0, 0, 0];
 
+// const nbAmps = 5;
 function getOutput() {
     return outputs[outputs.length - 1];
 }
@@ -25,24 +23,33 @@ function hasSameNumber(phases: number[]) {
     return (new Set(phases)).size !== phases.length;
 }
 
-for (const amp1Phase of phases) {
-    for (const amp2Phase of phases) {
-        for (const amp3Phase of phases) {
-            for (const amp4Phase of phases) {
-                for (const amp5Phase of phases) {
-                    const phases = [amp1Phase, amp2Phase, amp3Phase, amp4Phase, amp5Phase];
-                    if (hasSameNumber(phases)) {
-                        continue;
-                    }
-                    let output = calcOutput(phases, [...input]);
-                    if (output > highestThrust) {
-                        bestPhasesSetting = phases;
-                        highestThrust = output;
+function calcPart1() {
+    let highestThrust = 0;
+    let bestPhasesSetting = [0, 0, 0, 0, 0];
+
+    for (const amp1Phase of phases) {
+        for (const amp2Phase of phases) {
+            for (const amp3Phase of phases) {
+                for (const amp4Phase of phases) {
+                    for (const amp5Phase of phases) {
+                        const phases = [amp1Phase, amp2Phase, amp3Phase, amp4Phase, amp5Phase];
+                        if (hasSameNumber(phases)) {
+                            continue;
+                        }
+                        let output = calcOutput(phases, [...input]);
+                        if (output > highestThrust) {
+                            bestPhasesSetting = phases;
+                            highestThrust = output;
+                        }
                     }
                 }
             }
         }
     }
+    return [highestThrust, bestPhasesSetting]
 }
+
+// Part 1
+const [highestThrust, bestPhasesSetting] = calcPart1();
 
 console.log("Part 1: ", highestThrust, JSON.stringify(bestPhasesSetting));
