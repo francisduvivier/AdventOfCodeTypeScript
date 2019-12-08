d01f2e9c9804614256385b7ea76a0e7abf92148a
Francis Duvivier <francisduvivier@gmail.com>
2019-12-08 08:55:50 +0100

    🔧 Add script to run all days

---
+++ src/year2019/index.ts
@@ -0,0 +1,23 @@
+console.log('\nDay 1');
+import './day1/solution1';
+
+console.log('\nDay 2');
+import './day2/solution2';
+
+console.log('\nDay 3');
+import './day3/solution3';
+
+console.log('\nDay 4');
+import './day4/solution4';
+
+console.log('\nDay 5');
+import './day5/solution5';
+
+console.log('\nDay 6');
+import './day6/solution6';
+
+console.log('\nDay 7');
+import './day7/solution7';
+
+console.log('\nDay 8');
+import './day8/solution8';
