50bef1c4fbabc2c20b70a4ced28d6fb8aa48b4eb
Francis Duvivier <francisduvivier@gmail.com>
2019-12-17 06:00:19 +0100

    🐛 Fix day 16 repeat

--- src/year2019/day16/solution16.ts
+++ src/year2019/day16/solution16.ts
@@ -3,10 +3,10 @@ import {logAndPushSolution} from "../util/SolutionHandler";
 import {sum} from "../util/MapReduce";
 
 sum;
-const testInput1 = '80871224585914546619083218645595';
+export const testInput1 = '80871224585914546619083218645595';
 const input = '59750530221324194853012320069589312027523989854830232144164799228029162830477472078089790749906142587998642764059439173975199276254972017316624772614925079238407309384923979338502430726930592959991878698412537971672558832588540600963437409230550897544434635267172603132396722812334366528344715912756154006039512272491073906389218927420387151599044435060075148142946789007756800733869891008058075303490106699737554949348715600795187032293436328810969288892220127730287766004467730818489269295982526297430971411865028098708555709525646237713045259603175397623654950719275982134690893685598734136409536436003548128411943963263336042840301380655801969822';
 
-function repeat<T extends number>(basePattern: T[], times: number, maxLength?: number): T[] {
+function repeatPattern<T extends number>(basePattern: T[], times: number, maxLength?: number): T[] {
     let resultArray: T[] = [];
     for (let patval of basePattern) {
         for (let ip = 0; ip < times; ip++) {
@@ -19,8 +19,18 @@ function repeat<T extends number>(basePattern: T[], times: number, maxLength?: n
     return resultArray;
 }
 
-assert.deepEqual(repeat([0, 1, 0, -1], 1), [0, 1, 0, -1]);
-assert.deepEqual(repeat([0, 1, 0, -1], 3), [0, 0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1]);
+export function repeat<T extends number>(basePattern: T[], times: number, maxLength?: number): T[] {
+    let resultArray: T[] = [];
+    for (let time = 0; time < times; time++) {
+        resultArray.push(...basePattern);
+    }
+    return resultArray;
+}
+
+assert.deepEqual(repeatPattern([0, 1, 0, -1], 1), [0, 1, 0, -1]);
+assert.deepEqual(repeatPattern([1, 2, 3, 5], 5), [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 5, 5, 5, 5, 5,]);
+assert.deepEqual(repeat([1, 2, 3, 5], 5), [1, 2, 3, 5, 1, 2, 3, 5, 1, 2, 3, 5, 1, 2, 3, 5, 1, 2, 3, 5]);
+assert.deepEqual(repeat('5asdfa21654' as any, 5), '5asdfa216545asdfa216545asdfa216545asdfa216545asdfa21654'.split(''));
 
 
 function calcPattern(basePattern: SIGN[], repeatNb: number, maxLength?: number): SIGN[] {
@@ -46,17 +56,16 @@ assert.deepEqual(calcPattern([0, 1, 0, -1], 3, 7), [0, 0, 1, 1, 1, 0, 0]);
 assert.deepEqual(calcPattern([0, 1, 0, -1], 3, 12), [0, 0, 1, 1, 1, 0, 0, 0, -1, -1, -1, 0]);
 
 
-function part1(inputNb: string): string {
+export function part1(inputNb: string): string {
     const nbPhases = 100;
     const basePattern: SIGN[] = [0, 1, 0, -1];
-    const result = doFFT(nbPhases, inputNb.split('').map(Number), basePattern);
+    const result = doFFTOld(nbPhases, inputNb.split('').map(Number), basePattern, 1);
     return result.join('');
 }
 
 assert.deepEqual(part1(testInput1).slice(0, 8), '24176176');
-console.log('success 24176176');
-// assert.deepEqual(part1('19617804207202209144916044189917').slice(0, 8), '73745418');
-// assert.deepEqual(part1('69317163492948606335995924319873').slice(0, 8), '52432133');
+assert.deepEqual(part1('19617804207202209144916044189917').slice(0, 8), '73745418');
+assert.deepEqual(part1('69317163492948606335995924319873').slice(0, 8), '52432133');
 const solutions: string[] = [];
 logAndPushSolution(part1(input).slice(0, 8), solutions);
 
@@ -113,12 +122,12 @@ function getSumMap(currInput: number[]) {
     return sumMap;
 }
 
-function doFFT(nbPhases: number, unrepeatedStartInput: number[], basePattern: SIGN[], repeatInput = 1) {
+export function doFFTOld(nbPhases: number, unrepeatedStartInput: number[], basePattern: SIGN[], repeatInput = 1) {
     const startDate = Date.now();
     let lastShow: number = Date.now();
-    let currInput = repeat(unrepeatedStartInput, repeatInput);
+    let currInput = repeatPattern(unrepeatedStartInput, repeatInput);
     const fullInputLength = currInput.length;
-    console.log('fullInputLength', fullInputLength);
+    console.log('old fullInputLength', fullInputLength);
     const DEBUG = false;
     for (let phase = 0; phase < nbPhases; phase++) {
         const sumMap = getSumMap(currInput);
@@ -153,17 +162,39 @@ function doFFT(nbPhases: number, unrepeatedStartInput: number[], basePattern: SI
     return currInput;
 }
 
-function part2(inputNb: string): string {
+function doFFT(nbPhases: number, unrepeatedStartInput: number[], repeatInput, msgOffset: number = 0) {
+    const startDate = Date.now();
+    let lastShow: number = Date.now();
+    let currInput = repeat(unrepeatedStartInput, repeatInput);
+    const fullInputLength = currInput.length;
+    console.log('new fullInputLength', fullInputLength);
+    for (let phase = 0; phase < nbPhases; phase++) {
+        const sumMap: number[] = [];
+
+        for (let iForPat = currInput.length - 1; iForPat >= msgOffset - 1; iForPat--) {
+            sumMap[iForPat] = (sumMap[iForPat + 1] || 0) + currInput[iForPat];
+            currInput[iForPat] = Math.abs(sumMap[iForPat]) % 10
+        }
+        const currDate = Date.now();
+        if (currDate - lastShow > 1000) {
+            let timeDiffSinceStart = currDate - startDate;
+            let millisPerSum = (timeDiffSinceStart) / (phase + 1);
+            console.log('did phase ', (phase + 1), 'left: ' + (nbPhases - (phase + 1)), 'will take ' + (nbPhases - (phase + 1)) * millisPerSum / 1000, 'sec');
+            lastShow = currDate;
+        }
+    }
+    return currInput;
+}
+
+
+function part2(inputNb: string, repeatTimes = 10000): string {
     const msgOffset = Number(inputNb.slice(0, 7));
     const nbPhases = 100;
-    const basePattern: SIGN[] = [0, 1, 0, -1];
-    const repeatInputTimes = 10001;
-    const result = doFFT(nbPhases, inputNb.split('').map(Number), basePattern, repeatInputTimes);
-    console.log(result.join(''));
+    const result = doFFT(nbPhases, inputNb.split('').map(Number), repeatTimes, msgOffset);
     console.log(result.length, msgOffset, result.slice(msgOffset, msgOffset + 8).join(''), result.slice(msgOffset + 7, msgOffset + 7 + 8).join(''));
     return result.slice(msgOffset, msgOffset + 8).join('');
 }
 
-assert.deepEqual(part2('02935109699940807407585447034323'), '78725270');
-logAndPushSolution(part2(input).slice(0, 8), solutions);
-assert.deepEqual(part2('03036732577212944063491565474664'), '84462026');
+console.log(part2('03036732577212944063491565474664', 10000), '84462026');
+console.log(part2('02935109699940807407585447034323', 10000), '78725270');
+logAndPushSolution(part2(input, 10000), solutions);
