7b5e918200ae7cba0b97af52a0fec45c6ed609c9
Francis Duvivier <francisduvivier@gmail.com>
2020-12-12 14:43:11 +0100

    Implement new AssemblyRunner for Day 8

--- deno/year2020/day8/solution8.ts
+++ deno/year2020/day8/solution8.ts
@@ -1,25 +1,66 @@
 import { logAndPushSolution } from "../../year2019/util/SolutionHandler.ts";
 import { rInput } from "./input.ts";
-let intcodeRunner: IntcodeRunner;
+
+interface InstructionData {
+    code: keyof AssemblyRunner["instructionMap"]
+    args: number[]
+}
+
+class AssemblyRunner {
+    private mem = [];
+    private instructionPointer = 0;
+    private pointerHistory: number[] = [];
+    private readonly program: InstructionData[]
+    private readonly accumulator: Accumulator;
+    readonly instructionMap = {
+        'nop': () => {
+        },
+        'jmp': (args) => {
+            this.instructionPointer += args[0] - 1
+        },
+        'acc': (args) => {
+            this.accumulator.doAcc(args[0])
+        }
+    }
+
+    constructor(lines, accumulator: Accumulator) {
+        this.accumulator = accumulator;
+        this.program = lines.map(line => line.split(' ')).map(duo => ({
+            code: duo[0],
+            args: duo.slice(1).map(Number)
+        }) as InstructionData);
+    }
+
+    run(): number {
+        while (this.pointerHistory.indexOf(this.instructionPointer) == -1 && this.instructionPointer < this.program.length) {
+            this.pointerHistory.push(this.instructionPointer)
+            const instruction = this.program[this.instructionPointer]
+            this.instructionMap[instruction.code](instruction.args);
+            this.instructionPointer++;
+        }
+        if (this.instructionPointer < this.program.length) {
+            return -1;
+        }
+        return 1;
+    }
+}
+
+let intcodeRunner: AssemblyRunner;
 
 // Part 1
 
-class Accumulator implements IOHandler {
+class Accumulator {
     public count = 0;
 
-    doOutput(output: number | string): void {
+    doAcc(output: number): void {
         this.count += Number(output)
-    }
-
-    getInput(): number {
-        return 0;
     }
 }
 
 const solutionArray = [];
 let accumulator = new Accumulator();
 //Part 1
-intcodeRunner = new IntcodeRunner(rInput.split(/\n/), [], accumulator);
+intcodeRunner = new AssemblyRunner(rInput.split(/\n/), accumulator);
 intcodeRunner.run();
 logAndPushSolution(accumulator.count, solutionArray);
 
@@ -31,13 +72,13 @@ const mutations = input.map((v, i) => i).filter(i => input[i].match(/nop|jmp/)).
     const orig = copy[switchI];
     copy[switchI] = orig.replace('nop', 'jmp')
     if (orig == copy[switchI]) {
-        copy[switchI]= orig.replace('jmp', 'nop')
+        copy[switchI] = orig.replace('jmp', 'nop')
     }
-    return copy
+    return copy;
 })
 for (const mutation of mutations) {
     accumulator.count = 0;
-    intcodeRunner = new IntcodeRunner(mutation, [], accumulator);
+    intcodeRunner = new AssemblyRunner(mutation, accumulator);
     let result = intcodeRunner.run();
     if (result != -1) {
         logAndPushSolution(accumulator.count, solutionArray);
